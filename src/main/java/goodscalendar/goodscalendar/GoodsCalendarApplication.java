a0a086a706db26c0558f1016dae12e0fe4e25c57
Brogod97 <evergreen1286@gmail.com>
2023-12-07 15:13:52 +0900

    Init Spring Boot Project

---
+++ src/main/java/goodscalendar/goodscalendar/GoodsCalendarApplication.java
@@ -0,0 +1,13 @@
+package goodscalendar.goodscalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goodscalendar/goodscalendar/GoodsCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package goodscalendar.goodscalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
