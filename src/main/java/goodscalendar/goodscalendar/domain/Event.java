356b4cd041d1483660ae607556e654c1d6229774
Brogod97 <evergreen1286@gmail.com>
2023-12-15 16:21:19 +0900

    Lombok toString 어노테이션 추가

--- src/main/java/goodscalendar/goodscalendar/domain/Event.java
+++ src/main/java/goodscalendar/goodscalendar/domain/Event.java
@@ -1,14 +1,12 @@
 package goodscalendar.goodscalendar.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
