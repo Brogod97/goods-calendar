6b8e000a193b04c4ded449ff3a7a067d5173d7ec
Brogod97 <evergreen1286@gmail.com>
2024-02-14 19:02:38 +0900

    Misc: 불필요 주석 제거

--- src/main/java/goodscalendar/goodscalendar/respository/EventRepository.java
+++ src/main/java/goodscalendar/goodscalendar/respository/EventRepository.java
@@ -5,16 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-//    Event save(Event event);
-//    Optional<Event> findById(Long id);
-//    List<Event> findAll();
-
-
     List<Event> findByTitleContaining(String title);
 
     @Query("SELECT e FROM Event e WHERE SUBSTRING(e.startDate, 1, 4) = :year AND SUBSTRING(e.startDate, 6, 2) = :month")
